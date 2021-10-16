65e6392e0db2b5df52232be711b2910d71c870b1
atif9010 <atif9010@gmail.com>
2021-10-16 19:57:11 +0400

    1 file modified

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 document.querySelector('#userHit').addEventListener('click', userGame);
 
-
 let blackJackGame = {
-  'you': {'box': '#flex-box-container-1', 'score': '#your-score',},
-  'dealer': {'box': '#flex-box-container-2', 'score': '#bot-score',},
-  'cards' : ["2","3","4","5","6","7","8","9","10","A","J","K","Q"],
-  'cards-map' : [2,3,4,5,6,7,8,9,10,[1,10],10,10,10],
-  'wins' : 0,
-  'losses' : 0,
-  'draws' : 0,
-} 
+  you: { box: '#flex-box-container-1', score: '#your-score' },
+  dealer: { box: '#flex-box-container-2', score: '#bot-score' },
+  cards: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'J', 'K', 'Q'],
+  'cards-map': [2, 3, 4, 5, 6, 7, 8, 9, 10, [1, 10], 10, 10, 10],
+  wins: 0,
+  losses: 0,
+  draws: 0,
+};
 
 function userGame() {
-  console.log('user have clicked');
+  const YOU = blackJackGame.you;
 }
